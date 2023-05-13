665b869d860d40a481b0b5fabfbd6686a86f251c
CHOIHYUNAH <125236585+NO1CHOI@users.noreply.github.com>
2023-05-13 14:06:07 +0900

    Add files via upload

---
+++ postype/script/sub_2.js
@@ -0,0 +1,8 @@
+let slide = new Swiper('.swiper-container',{
+    direction:'horizontal',
+    autoplay:{delay:5000,},
+    navigation:{
+        nextEl:'.swiper-button-next',
+        prevEl:'.swiper-button-prev'
+        }
+    })
